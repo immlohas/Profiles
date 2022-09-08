98b7f2d2228759894040944632e689c693f84d64
Immlohas <48196021+immlohas@users.noreply.github.com>
2022-09-08 23:34:23 +0800

    Update FlushDNS.js

--- JavaScript/FlushDNS.js
+++ JavaScript/FlushDNS.js
@@ -1,3 +1,13 @@
+/*
+[Script]
+flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js
+// use "title" or "icon" or "color" or "server" in "argument":
+// flushDNS = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/zZPiglet/Task/master/asset/flushDNS.js,argument=title=DNS FLush&icon=arrow.clockwise&color=#3d3d5b&server=false
+
+[Panel]
+flushDNS = script-name=flushDNS,update-interval=600
+*/
+
 !(async () => {
     let panel = { title: "Flush DNS" },
         showServer = true,
